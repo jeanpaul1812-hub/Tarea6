38d6cde2999744d8c88bd6e7fe6b793082ca8f99
jeanpaul1812-hub <jeanpaul1812@gmail.com>
2026-09-05 22:35:19 -0400

    Delete src/main directory

--- src/main/java/com/trabajo6/jpa/CarRepository.java
+++
@@ -1,6 +0,0 @@
-package com.trabajo6.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<CarEntity, Integer> {
-}
--- src/main/java/com/trabajo6/jpa/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trabajo6.jpa;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    Optional<UserEntity> findByUsuario(String usuario);
-}
